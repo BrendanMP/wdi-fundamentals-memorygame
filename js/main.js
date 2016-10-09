366d0006d1ba50a1fda18e3a31297fd36101a4f6
brendanMP <brendan.m.pete@gmail.com>
2016-10-09 19:01:52 -0400

    Added if else to main.js.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 // Assigning values to card variables.
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
+
+var choiceOne = cardOne;
+var choiceTwo = cardFour;
+
+if (choiceOne === cardOne && choiceTwo === cardTwo) {
+    alert('It\'s a match');
+} else if (choiceOne === cardThree && choiceTwo === cardFour) {
+    alert('It\'s a match');
+} else {
+    alert('Sorry Try Again');
+}
