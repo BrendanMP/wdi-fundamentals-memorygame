8051fa42d0a9a897f84a3efe130462c06eab84ea
brendanMP <brendan.m.pete@gmail.com>
2016-10-14 15:30:45 -0400

    Added emoji & shuffle.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
 //(function(){})();
 
 var gameBoard = document.getElementById('game-board');
-var cardsValues = ["ace", "king", "queen", "jack"];
+var cardsValues = ["💩", "🔑", "🔥", "💸"];
 var card;
 var deck = [];
 var inPlay = [];
 
+function shuffleDeck() {
+    // Fisher–Yates Shuffle
+    var m = deck.length, t, i;
+    while (m) {
+        i = Math.floor(Math.random() * m--);
+        t = deck[m];
+        deck[m] = deck[i];
+        deck[i] = t;
+    }
+    return deck;
+}
 
 function createDeck() {
     for (var i = 0; i < cardsValues.length; i++) {
         deck.push(cardsValues[i], cardsValues[i]);
     }
+    shuffleDeck();
 }
 
 function createCard() {
@@ -23,7 +35,9 @@ function clearCards() {
     var card_1 = document.getElementById(inPlay[0].card.id);
     var card_2 = document.getElementById(inPlay[1].card.id);
     card_1.innerHTML = '';
+    card_1.className = 'card cleared';
     card_2.innerHTML = '';
+    card_2.className = 'card cleared';
     inPlay.length = 0;
 }
 
@@ -32,8 +46,11 @@ function addMatch() {
     var card_2 = document.getElementById(inPlay[1].card.id);
     card_1.innerHTML = inPlay[0].card.attribute;
     card_1.removeEventListener('click', flipCard);
+    card_1.className = 'card selected matched';
     card_2.innerHTML = inPlay[1].card.attribute;
     card_2.removeEventListener('click', flipCard);
+    card_2.className = 'card selected matched second';
+
     inPlay.length = 0;
 }
 
@@ -48,23 +65,22 @@ function dealCards() {
 
 function flipCard() {
     inPlay.push({card: {id: this.id, attribute: this.getAttribute('data')}});
-
+    this.className = 'card selected';
     if (inPlay.length <= 1) {
         console.log('first card - ' + inPlay[0].card.attribute);
         this.innerHTML = inPlay[0].card.attribute;
     } else if (inPlay.length === 2) {
         console.log('second card - ' + inPlay[1].card.attribute);
         this.innerHTML = inPlay[1].card.attribute;
-
         if (inPlay[0].card.id === this.id) {
             console.log('- same card!');
-            setTimeout(clearCards, 1500);
+            inPlay.pop();
         } else if (inPlay[0].card.attribute === inPlay[1].card.attribute) {
             console.log('- match!');
             addMatch();
         } else {
             console.log('- no match!');
-            setTimeout(clearCards, 1000);
+            setTimeout(clearCards, 600);
         }
 
     }
